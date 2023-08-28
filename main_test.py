6568de2d2b3f5994320d7f1c4763d69d2afe1693
mjy961228 <67363657+mjy961228@users.noreply.github.com>
2023-08-29 03:25:23 +0800

    Add files via upload

---
+++ main_test.py
@@ -0,0 +1,188 @@
+import argparse
+
+import cv2
+import glob
+import numpy as np
+from collections import OrderedDict
+import os
+import torch
+import requests
+import time
+from models.network_lnsnet import LNSNet as net
+from utils import utils_image as util
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--task', type=str, default='gray_dn', help='gray_dn, color_dn, jpeg_car')
+    parser.add_argument('--scale', type=int, default=1, help='scale factor: 1, 2, 3, 4, 8') # 1 for dn and jpeg car
+    parser.add_argument('--noise', type=int, default=15, help='noise level: 15, 25, 50')
+    parser.add_argument('--model_path', type=str,
+                        default=' model_zoo/gray_noise15.pth')
+    parser.add_argument('--folder_lq', type=str, default=None, help='input low-quality test image folder')
+    parser.add_argument('--folder_gt', type=str, default='testsets/Set12', help='input ground-truth test image folder')
+    parser.add_argument('--tile', type=int, default=None, help='Tile size, None for no tile during testing (testing as a whole)')
+    parser.add_argument('--tile_overlap', type=int, default=32, help='Overlapping of different tiles')
+    args = parser.parse_args()
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    if os.path.exists(args.model_path):
+        print(f'loading model from {args.model_path}')
+        
+    model = define_model(args)
+    model.eval()
+    model = model.to(device)
+
+    # setup folder and path
+    folder, save_dir, border, window_size = setup(args)
+    os.makedirs(save_dir, exist_ok=True)
+    test_results = OrderedDict()
+    test_results['psnr'] = []
+    test_results['ssim'] = []
+    test_results['psnr_y'] = []
+    test_results['ssim_y'] = []
+    test_results['psnr_b'] = []
+    psnr, ssim, psnr_y, ssim_y, psnr_b = 0, 0, 0, 0, 0
+
+    for idx, path in enumerate(sorted(glob.glob(os.path.join(folder, '*')))):
+        # read image
+        imgname, img_lq, img_gt = get_image_pair(args, path)
+        img_lq = np.transpose(img_lq if img_lq.shape[2] == 1 else img_lq[:, :, [2, 1, 0]], (2, 0, 1))
+        img_lq = torch.from_numpy(img_lq).float().unsqueeze(0).to(device)
+
+        # inference
+        with torch.no_grad():
+            # pad input image to be a multiple of window_size
+            _, _, h_old, w_old = img_lq.size()
+            h_pad = (h_old // window_size + 1) * window_size - h_old
+            w_pad = (w_old // window_size + 1) * window_size - w_old
+            img_lq = torch.cat([img_lq, torch.flip(img_lq, [2])], 2)[:, :, :h_old + h_pad, :]
+            img_lq = torch.cat([img_lq, torch.flip(img_lq, [3])], 3)[:, :, :, :w_old + w_pad]
+
+            start_time = time.time()
+            output = test(img_lq, model, args, window_size)
+            elapsed_time = time.time() - start_time
+            print(' %2.4f second' % ( elapsed_time))
+
+            output = output[..., :h_old * args.scale, :w_old * args.scale]
+
+        # save image
+        output = output.data.squeeze().float().cpu().clamp_(0, 1).numpy()
+        if output.ndim == 3:
+            output = np.transpose(output[[2, 1, 0], :, :], (1, 2, 0))  # CHW-RGB to HCW-BGR
+        output = (output * 255.0).round().astype(np.uint8)  # float32 to uint8
+        cv2.imwrite(f'{save_dir}/{imgname}_lnsnet.png', output)
+
+        # evaluate psnr/ssim/psnr_b
+        if img_gt is not None:
+            img_gt = (img_gt * 255.0).round().astype(np.uint8)  # float32 to uint8
+            img_gt = img_gt[:h_old * args.scale, :w_old * args.scale, ...]  # crop gt
+            img_gt = np.squeeze(img_gt)
+
+            psnr = util.calculate_psnr(output, img_gt, border=border)
+            ssim = util.calculate_ssim(output, img_gt, border=border)
+            test_results['psnr'].append(psnr)
+            test_results['ssim'].append(ssim)
+            if img_gt.ndim == 3:  # RGB image
+                output_y = util.bgr2ycbcr(output.astype(np.float32) / 255.) * 255.
+                img_gt_y = util.bgr2ycbcr(img_gt.astype(np.float32) / 255.) * 255.
+                psnr_y = util.calculate_psnr(output_y, img_gt_y, border=border)
+                ssim_y = util.calculate_ssim(output_y, img_gt_y, border=border)
+                test_results['psnr_y'].append(psnr_y)
+                test_results['ssim_y'].append(ssim_y)
+            print('Testing {:d} {:20s} - PSNR: {:.2f} dB; SSIM: {:.4f}; '
+                  'PSNR_Y: {:.2f} dB; SSIM_Y: {:.4f}; '
+                  'PSNR_B: {:.2f} dB.'.
+                  format(idx, imgname, psnr, ssim, psnr_y, ssim_y, psnr_b))
+        else:
+            print('Testing {:d} {:20s}'.format(idx, imgname))
+
+    # summarize psnr/ssim
+    if img_gt is not None:
+        ave_psnr = sum(test_results['psnr']) / len(test_results['psnr'])
+        ave_ssim = sum(test_results['ssim']) / len(test_results['ssim'])
+        print('\n{} \n-- Average PSNR/SSIM(RGB): {:.2f} dB; {:.4f}'.format(save_dir, ave_psnr, ave_ssim))
+        if img_gt.ndim == 3:
+            ave_psnr_y = sum(test_results['psnr_y']) / len(test_results['psnr_y'])
+            ave_ssim_y = sum(test_results['ssim_y']) / len(test_results['ssim_y'])
+            print('-- Average PSNR_Y/SSIM_Y: {:.2f} dB; {:.4f}'.format(ave_psnr_y, ave_ssim_y))
+
+def define_model(args):
+
+    if args.task == 'gray_dn':
+        model = net(upscale=1, in_chans=1, img_size=128, window_size=8,
+                    img_range=1., depths=[6,6,6,6,6,6], embed_dim=180, num_heads=[6,6,6,6,6, 6],
+                    upsampler='', resi_connection='1conv')
+        param_key_g = 'params'
+
+    # 005 color image denoising
+    elif args.task == 'color_dn':
+        model = net(upscale=1, in_chans=3, img_size=128, window_size=8,
+                    img_range=1., depths=[6, 6, 6, 6, 6, 6], embed_dim=180, num_heads=[6, 6, 6, 6, 6, 6],
+                    mlp_ratio=2, upsampler='', resi_connection='1conv')
+        param_key_g = 'params'
+
+    pretrained_model = torch.load(args.model_path)
+    model.load_state_dict(pretrained_model[param_key_g] if param_key_g in pretrained_model.keys() else pretrained_model, strict=True)
+        
+    return model
+
+
+def setup(args):
+    if args.task in ['gray_dn', 'color_dn']:
+        save_dir = f'results/lnsnet_{args.task}_noise{args.noise}'
+        folder = args.folder_gt
+        border = 0
+        window_size = 8
+
+
+    return folder, save_dir, border, window_size
+
+
+def get_image_pair(args, path):
+    (imgname, imgext) = os.path.splitext(os.path.basename(path))
+
+    if args.task in ['gray_dn']:
+        img_gt = cv2.imread(path, cv2.IMREAD_GRAYSCALE).astype(np.float32) / 255.
+        np.random.seed(seed=0)
+
+        img_lq = img_gt + np.random.normal(0, args.noise / 255., img_gt.shape)
+
+        img_gt = np.expand_dims(img_gt, axis=2)
+        img_lq = np.expand_dims(img_lq, axis=2)
+
+    return imgname, img_lq, img_gt
+
+
+def test(img_lq, model, args, window_size):
+    if args.tile is None:
+        # test the image as a whole
+        output = model(img_lq)
+    else:
+        # test the image tile by tile
+        b, c, h, w = img_lq.size()
+        tile = min(args.tile, h, w)
+        assert tile % window_size == 0, "tile size should be a multiple of window_size"
+        tile_overlap = args.tile_overlap
+        sf = args.scale
+
+        stride = tile - tile_overlap
+        h_idx_list = list(range(0, h-tile, stride)) + [h-tile]
+        w_idx_list = list(range(0, w-tile, stride)) + [w-tile]
+        E = torch.zeros(b, c, h*sf, w*sf).type_as(img_lq)
+        W = torch.zeros_like(E)
+
+        for h_idx in h_idx_list:
+            for w_idx in w_idx_list:
+                in_patch = img_lq[..., h_idx:h_idx+tile, w_idx:w_idx+tile]
+                out_patch = model(in_patch)
+                out_patch_mask = torch.ones_like(out_patch)
+
+                E[..., h_idx*sf:(h_idx+tile)*sf, w_idx*sf:(w_idx+tile)*sf].add_(out_patch)
+                W[..., h_idx*sf:(h_idx+tile)*sf, w_idx*sf:(w_idx+tile)*sf].add_(out_patch_mask)
+        output = E.div_(W)
+
+    return output
+
+if __name__ == '__main__':
+    main()
